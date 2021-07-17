4cb8addbd22ffccaee0c1fece562a9040f3ea959
J-Babin <39302514+J-Babin@users.noreply.github.com>
2021-07-17 16:40:55 -0500

    start app

---
+++ Components/Cards.jsx
@@ -0,0 +1,10 @@
+import { View, TouchableHighlight } from 'react-native'
+
+export default function card () {
+    return(
+        <View>
+            <TouchableHighlight>
+            </TouchableHighlight>
+        </View>
+    )
+}
