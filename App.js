1f1c97b12b029dad7cae83ab9f4341f8e2a8cca8
J-Babin <39302514+J-Babin@users.noreply.github.com>
2021-07-18 08:44:37 -0500

    affichage du dos des cartes

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// import components 
+import Cards from './Components/Cards';
+
 export default function App() {
+
+  const map = () => {
+    let map_def = [];
+    let row = []
+
+    
+      for (let i = 0; i < 4; i++){
+      for (let j = 0; j <= 4 ; j++){
+        row [j] = { "value" : Math.random(0,9),}
+        if(j == 4){
+          map_def.push(row)
+          row = []
+        }
+      }
+    }
+    return map_def 
+  }
+    
+
+  const renderCards = (cards) => {
+    let test = cards.map((card, index) => {
+      console.log(card)
+      return (
+        <Cards key={index} />
+      );
+    });
+    return test
+  }
+
+
+  let row  = map();
+
+
+  let result;
+  if(row){
+ 
+    result = row.map((cards, index) => {
+      console.log(cards)
+      console.log("result ")
+      return (
+        <View key={index} style={styles.row} >
+           {renderCards(cards)} 
+        </View>
+      )
+    })
+  }
+
+   
+  
   return (
     <View style={styles.container}>
-      
+      <View style={styles.body}>
+        {result}
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: '#fff'
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  body: {
+    flex: 18,
+    justifyContent: 'space-between',
+    marginTop: 200,
+    marginBottom: 100,
+    marginLeft: 30,
+    marginRight: 30,
+  }
+
 });
--- Components/Cards.jsx
+++ Components/Cards.jsx
@@ -1,9 +1,22 @@
-import { View, TouchableHighlight } from 'react-native'
+import React from 'react';
+import { View, TouchableHighlight } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function card () {
+
+    const touch = () => {
+        console.warn("toucher")
+    }
     return(
         <View>
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => { touch() }}>
+                <View>
+                    <FontAwesome5
+                        name="question"
+                        size={35}
+                        color="black"
+                    />
+                </View>
             </TouchableHighlight>
         </View>
     )
